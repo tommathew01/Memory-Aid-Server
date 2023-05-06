fde0f64d6c12a99d5ab465545530c1f004e4e6e4
Tom Mathew <tomvallayil@gmail.com>
2023-05-07 00:56:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 app = Flask(__name__)
 
 @app.route('/')
@@ -6,6 +6,13 @@ def hello_world():
     return 'welcome to our service'
 
 
+@app.route('/summarize', methods=['POST'])
+def sq():
+    content_type = request.headers.get('Content-Type')
+    if (content_type == 'application/json'):
+        json = request.json
+        txt = json["text"]
+        return jsonify({'text': txt})
 
 
 
